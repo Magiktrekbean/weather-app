62060304ca2d4cf83ea3479f7b13797ac897735e
Magiktrekbean <dlockettkyla@gmail.com>
2025-01-20 14:43:35 -0500

    added h1 response to rest of users

--- script.js
+++ script.js
@@ -6,4 +6,6 @@ if (name.toLowerCase() === "patrick") {
 }
 if (name.toLowerCase() === "patrick") {
   document.getElementById("output").innerHTML = "I miss you Tuna🍥";
+} else {
+  document.getElementById("output").innerHTML = "Thanks" + name + "❣";
 }
