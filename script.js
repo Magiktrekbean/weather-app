e84222e296132b5cb0897e7ab8bff5b69f9a09af
Magiktrekbean <dlockettkyla@gmail.com>
2025-01-20 20:43:31 -0500

    added personalized message for dad

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let name = prompt("Hi! What's your name?");
-if (name.toLowerCase() === "patrick") {
-  alert("Hi! Snickerdoodle cupcake loverman!😘");
-} else {
-  alert("Hi " + name + "! Welcome!");
-}
-if (name.toLowerCase() === "patrick") {
-  document.getElementById("output").innerHTML = "I miss you Tuna🍥";
-} else {
-  document.getElementById("output").innerHTML = "Thanks " + name + "❣";
+if (name) {
+  if (name.toLowerCase() === "patrick") {
+    alert("Hi! Snickerdoodle cupcake loverman!😘");
+    document.getElementById("output").innerHTML = "I miss you Tuna🍥";
+  } else if (name.toLowerCase() === "clyde") {
+    alert(
+      "Psalm 103:13 As a father has compassion on his children, so the Lord has compassion on those who fear him."
+    );
+    document.getElementById("output").innerHTML = "Hi Dad👨🏿 I love you💖!";
+  } else {
+    alert("Hi " + name + "! Welcome!");
+    document.getElementById("output").innerHTML = "Thanks " + name + "❣";
+  }
 }
